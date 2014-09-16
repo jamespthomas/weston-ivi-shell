c653575f2054e0d3cc8fdd56d9c74ad76b9449d0
Jonathan Maw <jonathan.maw@codethink.co.uk>
2014-09-16 14:45:21 +0000

    Implement slider subdivision press/motion/release/enter/leave behaviour

--- ivi-input-calibration/src/calibration-subdivision-slider.c
+++ ivi-input-calibration/src/calibration-subdivision-slider.c
@@ -21,6 +21,7 @@
 
 #include <weston/compositor.h>
 
+#include "calibration-util.h"
 #include "calibration-subdivision-slider.h"
 
 int
@@ -65,12 +66,51 @@ calibration_subdivision_slider_parse_entry(
     return retval;
 }
 
+static void
+send_slider_axis(struct calibration_subdivision_slider *slider_sub,
+                 struct weston_seat *seat, uint32_t time,
+                 wl_fixed_t x, wl_fixed_t y)
+{
+    float axis_value, slider_position;
+    if (slider_sub->orientation == CALIBRATION_SLIDER_HORIZONTAL) {
+        uint32_t min = slider_sub->subdivision.top_left_x;
+        uint32_t max = slider_sub->subdivision.bottom_right_x;
+        slider_position = (wl_fixed_to_double(x) - min) / (float) (max - min);
+    } else if (slider_sub->orientation == CALIBRATION_SLIDER_VERTICAL) {
+        uint32_t min = slider_sub->subdivision.top_left_y;
+        uint32_t max = slider_sub->subdivision.bottom_right_y;
+        /* Vertical sliders are maximum-value at the top,
+         * minimum-value at the bottom */
+        slider_position = 1.0f - ((wl_fixed_to_double(y) - min)
+                                 / (float) (max - min));
+    } else {
+        weston_log("%s: Slider %s has unknown orientation '%d'\n",
+                   __FUNCTION__, slider_sub->subdivision.name,
+                   slider_sub->orientation);
+	return;
+    }
+    axis_value = slider_position * (slider_sub->maximum_value
+                                    - slider_sub->minimum_value);
+    notify_axis(seat, time, slider_sub->subdivision.id,
+                wl_fixed_from_double(axis_value));
+}
+
 void
 calibration_subdivision_slider_handle_down(
                 struct calibration_subdivision_slider *slider_sub,
                 struct weston_seat *seat, int is_pointer, uint32_t button,
                 uint32_t time, wl_fixed_t x, wl_fixed_t y)
 {
+    struct calibration_subdivision *sub = &slider_sub->subdivision;
+    int slot = is_pointer ? CALIBRATION_POINTER_SLOT : button;
+    if (!calibration_subdivision_get_slot_pressed(sub, slot)) {
+        struct weston_pointer_grab *grab = &seat->pointer->default_grab;
+        grab->interface->button(grab, time, sub->id,
+                                WL_POINTER_BUTTON_STATE_PRESSED);
+
+        send_slider_axis(slider_sub, seat, time, x, y);
+        calibration_subdivision_set_slot_pressed(sub, slot, 1);
+    }
 }
 
 void
@@ -79,6 +119,14 @@ calibration_subdivision_slider_handle_up(
                 struct weston_seat *seat, int is_pointer, uint32_t button,
                 uint32_t time, wl_fixed_t x, wl_fixed_t y)
 {
+    struct calibration_subdivision *sub = &slider_sub->subdivision;
+    int slot = is_pointer ? CALIBRATION_POINTER_SLOT : button;
+    if (calibration_subdivision_get_slot_pressed(sub, slot)) {
+        struct weston_pointer_grab *grab = &seat->pointer->default_grab;
+        grab->interface->button(grab, time, sub->id,
+                                WL_POINTER_BUTTON_STATE_RELEASED);
+        calibration_subdivision_set_slot_pressed(sub, slot, 0);
+    }
 }
 
 void
@@ -87,6 +135,7 @@ calibration_subdivision_slider_handle_motion(
                 struct weston_seat *seat, int is_pointer, uint32_t button,
                 uint32_t time, wl_fixed_t x, wl_fixed_t y)
 {
+    send_slider_axis(slider_sub, seat, time, x, y);
 }
 
 void
@@ -102,4 +151,12 @@ void calibration_subdivision_slider_handle_leave(
                 struct weston_seat *seat, int is_pointer, uint32_t button,
                 uint32_t time, wl_fixed_t x, wl_fixed_t y)
 {
+    struct calibration_subdivision *sub = &slider_sub->subdivision;
+    int slot = is_pointer ? CALIBRATION_POINTER_SLOT : button;
+    if (calibration_subdivision_get_slot_pressed(sub, slot)) {
+        struct weston_pointer_grab *grab = &seat->pointer->default_grab;
+        grab->interface->button(grab, time, sub->id, 
+                                WL_POINTER_BUTTON_STATE_RELEASED);
+        calibration_subdivision_set_slot_pressed(sub, slot, 0);
+    }
 }
